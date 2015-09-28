9b4f41f22c6cfe7d9852ac0f629cf04d150f91d5
buffpojken <daniel@sykewarrior.com>
2015-09-29 00:58:18 +0200

    now publishing events

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-var Bleacon = require('bleacon');
-var mqtt    = require('mqtt');
-var client  = mqtt.connect('mqtt://192.168.0.113');
+var Bleacon       = require('bleacon');
+var mqtt          = require('mqtt');
+var underscore    = require('underscore'); 
+var client        = mqtt.connect('mqtt://192.168.0.113');
+
+var throttled_publish = _.throttle(function(uuid){
+  client.publish('btle', uuid); 
+}, 12000); 
 
 Bleacon.on('discover', function(bleacon){
-	console.log(bleacon.uuid);
+  console.log("monkey");
+  console.log(bleacon.uuid);
+  throttled_publish(bleacon.uuid)
 });
 
-Bleacon.startScanning("3DDD37E3-86D1-487E-9F4A-BA1DBF64D274", true); 
+Bleacon.startScanning("3ddd37e386d1487e9f4aba1dbf64d274");
+
+console.log("sanning");
